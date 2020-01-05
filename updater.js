6884ddeedaa575a582c33b616cb3189433e53849
Baizey <Baizey@users.noreply.github.com>
2020-01-05 05:27:44 +0100

    Update updater.js

--- updater.js
+++ updater.js
@@ -1,4 +1,9 @@
 let baseUrl = 'https://raw.githubusercontent.com/Baizey/BitBurner/master/';
+let url = path => `${baseUrl}${path}`;
+
+async function get(ns, url, name) {
+    return await ns.wget(url, name);
+}
 
 export async function main(ns) {
     let files = [
@@ -11,7 +16,7 @@ export async function main(ns) {
         'weaken.script'
     ];
     for (let i in files)
-        await ns.wget(`${baseUrl}${files[i]}`);
-    await ns.wget(`${baseUrl}updater.js`);
+        await ns.wget(`${baseUrl}${files[i]}`, files[i]);
+    await ns.wget(`${baseUrl}updater.js`, 'updater.js');
     ns.tprint('<span style="color:white">Done updating!</span>');
 }
