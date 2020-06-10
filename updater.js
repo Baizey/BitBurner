81d5a2deae63f1cecaa20c665990106adcd160dd
Baizey <Baizey@users.noreply.github.com>
2020-06-10 22:34:18 +0200

    Update updater.js

--- updater.js
+++ updater.js
@@ -9,6 +9,7 @@ export async function main(ns) {
     ns.tprint(`<span style="color:lightgrey">Updating ${host}</span>`);
 
     let files = [
+        'monohack.js',
         'purchaser.js',
         'script.js',
         'cheaphack.js',
