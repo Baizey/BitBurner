994ea0cde7f5b0c78e058ce6304d1d8bb689db98
Baizey <Baizey@users.noreply.github.com>
2020-06-10 13:17:36 +0200

    .

--- cheaphack.js
+++ cheaphack.js
@@ -1,3 +1,5 @@
+import {Runner} from 'utils.js'
+import {asPercent} from 'utils.js'
 import {Server} from 'server.js'
 
 /**
@@ -5,17 +7,54 @@ import {Server} from 'server.js'
  * @returns {Promise<void>}
  */
 export async function main(ns) {
-    const target = ns.args[0];
+    const targetName = ns.args[0];
+    const type = ns.args[1];
     const allServers = Server.get(ns);
+    const target = allServers.filter(e => e.name === targetName)[0];
+    ns.disableLog('sleep');
 
-    while (true) {
+    let readyOn = Date.now();
+
+    const condition = findCondtionFunc(type, target, ns);
+    const time = findTimeFunc(type, target, ns);
+    const runner = findRunnerFunc(type, target, ns);
+
+    while (condition()) {
+        const growTime = ns[time](target.name) * 1000;
+        const startTime = Date.now() + 10000;
+        readyOn = startTime + growTime + 1000;
+
         for (let server of allServers) {
             if (!server.hasRoot || server.name === 'home') continue;
-            const used = ns.getScriptRam('hack.script')
+            const used = 1.75;
             const threads = Math.floor(server.freeRam / used);
-            if (threads < 1) continue;
-            await ns.exec('hack.script', server.name, threads, target)
+            await Runner[runner](ns, threads, target.name, startTime, server.name);
         }
-        await ns.sleep(10000);
+
+        await ns.sleep(8000);
+        ns.clearLog();
+        ns.print(`Waiting ${((readyOn - Date.now()) / 1000).toFixed(2)} seconds`);
+        ns.print(`At: ${asPercent(target.moneyAvail / target.moneyMax)}`);
+        while (Date.now() < readyOn)
+            await ns.sleep(1000);
+        ns.clearLog();
     }
+}
+
+function findRunnerFunc(type) {
+    if (type === 'grow') return 'runGrow';
+    if (type === 'hack') return 'runHack';
+    return 'runWeaken';
+}
+
+function findCondtionFunc(type, target) {
+    if (type === 'grow') return () => !target.hasMaxMoney;
+    if (type === 'hack') return () => target.moneyAvail > 1000000;
+    return () => !target.hasMinSecurity
+}
+
+function findTimeFunc(type, target, ns) {
+    if (type === 'grow') return () => ns.getGrowTime(target.name);
+    if (type === 'hack') return () => ns.getHackTime(target.name);
+    return () => ns.getWeakenTime(target.name);
 }
