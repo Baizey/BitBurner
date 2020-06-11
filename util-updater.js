09ca51fd73847d2edc0d52eb5316b186b5a1f9b5
Baizey <Baizey@users.noreply.github.com>
2020-06-11 20:50:58 +0200

    Update util-updater.js

--- util-updater.js
+++ util-updater.js
@@ -1,3 +1,5 @@
+import {cmd} from "util-inject.js";
+
 let baseUrl = 'https://raw.githubusercontent.com/Baizey/BitBurner/renewal/';
 let _ns, _host, _verbose;
 
@@ -14,6 +16,15 @@ export async function main(ns) {
 
     _verbose = args.indexOf('-v') >= 0;
 
+    if (args.indexOf('-a') >= 0) {
+        ns.tprint(`<span style="color:lightgrey">Adding aliases</span>`);
+        cmd(ns, 'alias -g update="run util-updater.js"');
+        cmd(ns, 'alias -g crack="run util-crack.js"');
+        cmd(ns, 'alias -g server="run util-purchaser.js"');
+        cmd(ns, 'alias -g scan="run util-server.js"');
+        ns.tprint(`<span style="color:lightgrey">Adding aliases</span>`);
+    }
+
     if (args.indexOf('-r') >= 0) {
         ns.tprint(`<span style="color:lightgrey">Removing old files</span>`);
         ns.ls(_host)
@@ -21,7 +32,8 @@ export async function main(ns) {
             .forEach(e => ns.rm(e, _host));
     }
 
-    ns.tprint(`<span style="color:lightgrey">Updating ${_host}</span>`);
+
+    ns.tprint(`<span style="color:lightgrey">Hostname ${_host}</span>`);
 
     await update('Utils', [
         'runner',
