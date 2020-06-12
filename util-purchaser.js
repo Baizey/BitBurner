61d3c4b37f3e19b7fa65197235e8cfce037275b1
Baizey <Baizey@users.noreply.github.com>
2020-06-12 16:35:21 +0200

    Update util-purchaser.js

--- util-purchaser.js
+++ util-purchaser.js
@@ -5,6 +5,7 @@ import {asFormat} from "util-utils.js";
  * @returns {Promise<void>}
  */
 export async function main(ns) {
+    await ns.sleep(4000);
     if (ns.args[0] === 'delete')
         if (ns.args[1] === 'all')
             ns.getPurchasedServers().forEach(s => ns.deleteServer(s))
@@ -14,14 +15,14 @@ export async function main(ns) {
             ns.tprint(`<span style="color:grey">Delete needs arg for hostname or 'all' to delete all</span>`);
     else if (ns.args[0] === 'buy') {
         if (ns.args[1] && ns.args[1] > 0) {
-            ns.purchaseServer(ns.args[2] || 's', Math.pow(2, ns.args[1] - 0));
+            ns.purchaseServer(ns.args[2] || 's', ns.args[1] - 0);
         } else {
             const money = ns.getServerMoneyAvailable('home')
             let result = 1;
             while (Math.pow(2, result) * 55000 < money)
                 result++;
             result--;
-            ns.tprint(`<span style="color:grey">Highest amount to buy with is ${result}, which is ${Math.pow(2, result)} ram and $${asFormat(55000 * Math.pow(2, result))}</span>`);
+            ns.tprint(`<span style="color:grey">Highest amount to buy with ${Math.pow(2, result)} GB arm and $${asFormat(55000 * Math.pow(2, result))}</span>`);
         }
     } else
         ns.tprint(`<span style="color:grey">First arg needs to be buy or delete</span>`);
