c584951108c9c23dfa49919cda970dba3ffc602e
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2021-12-19 18:23:30 +0100

    .

---
+++ automater.js
@@ -0,0 +1,37 @@
+﻿/**
+ * @param {import("Ns").NS } ns
+ * @returns {void}
+ */
+export async function main(ns) {
+    const [last] = ns.args;
+
+    switch (last) {
+        case mode.startup:
+            throw 'Not implemented'
+        case mode.running:
+            throw 'Not implemented'
+        case mode.endless:
+            throw 'Not implemented'
+    }
+
+}
+
+export const mode = {
+    // 8 GB on home
+    startup: 'startup',
+
+    // > 100 GB on home
+    running: 'running',
+
+    // > 1000 GB on home
+    endless: 'endless',
+}
+
+/**
+ * @param {{servers: any[]}} data
+ * @param {any[]} args
+ * @returns {*[]}
+ */
+export function autocomplete(data, args) {
+    return [...Object.values(mode)];
+}
--- updater.js
+++ updater.js
@@ -1,14 +1,21 @@
 const baseUrl = 'https://raw.githubusercontent.com/Baizey/BitBurner/1.2.0/';
 
-const files = ['scan.js', 'connect.js', 'updater.js'];
+const files = ['automater.js', 'scan.js', 'connect.js', 'updater.js'];
 
 /** @param {import("Ns").NS } ns */
 export async function main(ns) {
+    await update(ns);
+    await update(ns);
+    ns.tprint('Done updating!');
+}
+
+
+/** @param {import("Ns").NS } ns */
+async function update(ns) {
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         ns.rm(file);
         await ns.wget(`${baseUrl}${file}`, file);
         ns.print(`Got ${file} (${i + 1} / ${files.length})`)
     }
-    ns.tprint('Done updating!');
 }
