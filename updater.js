9b14a345ae3fb9d32fc65f376906908f945e2b3e
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2021-12-19 06:43:28 +0100

    .

--- updater.js
+++ updater.js
@@ -1,8 +1,9 @@
 const baseUrl = 'https://raw.githubusercontent.com/Baizey/BitBurner/1.2.0/';
 
+const files = ['scan.js'];
+
 /** @param {import("Ns").NS } ns */
 export async function main(ns) {
-    let files = ['scan.js'];
     for (let i in files) await ns.wget(`${baseUrl}${files[i]}`, files[i]);
     await ns.wget(`${baseUrl}updater.js`, 'updater.js');
     ns.tprint('Done updating!');
