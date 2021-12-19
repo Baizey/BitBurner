6c913d504c467da02e2df5e5e8b733cfb3daea0b
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2021-12-19 06:50:48 +0100

    .

--- updater.js
+++ updater.js
@@ -6,8 +6,9 @@ const files = ['scan.js', 'connect.js', 'updater.js'];
 export async function main(ns) {
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
+        ns.rm(file);
         await ns.wget(`${baseUrl}${file}`, file);
-        ns.print(`Got ${file} (${i} / ${files.length})`)
+        ns.tprint(`Got ${file} (${i + 1} / ${files.length})`)
     }
     ns.tprint('Done updating!');
 }
