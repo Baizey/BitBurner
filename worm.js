55a919f6e94da157d13cf33a66515cec7e336a76
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2021-12-20 03:37:07 +0100

    .

--- worm.js
+++ worm.js
@@ -1,8 +1,10 @@
 ﻿import {getServers} from "./scan.js";
+import {files} from "./updater";
 
 /** @param {import("Ns").NS } ns */
 export async function main(ns) {
     const servers = getServers(ns);
+
     for (let server of servers.map(e => e.name)) {
         await tryCatchIgnore(() => ns.brutessh(server))
         await tryCatchIgnore(() => ns.relaysmtp(server))
@@ -10,6 +12,10 @@ export async function main(ns) {
         await tryCatchIgnore(() => ns.ftpcrack(server))
         await tryCatchIgnore(() => ns.sqlinject(server))
         await tryCatchIgnore(() => ns.nuke(server))
+    }
+    
+    for (const server of servers) {
+        await ns.scp(files.map(e => `${e}.js`), ns.getHostname(), server.name);
     }
 }
 
