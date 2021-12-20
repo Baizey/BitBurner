aafe44b8f8da1fd5002bc313044e4ae39541ce3d
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2021-12-20 09:24:04 +0100

    -

---
+++ auto_weaken.js
@@ -0,0 +1,49 @@
+﻿import {getServers} from "./scan.js";
+import {weakenProgress} from "./constants.js";
+
+/** {import("Ns").NS } ns */
+let ns;
+
+/**
+ * @param {import("Ns").NS } _ns
+ * @returns {void}
+ */
+export async function main(_ns) {
+    ns = _ns;
+    const servers = getServers(ns).map(e => e.name);
+    while (true) {
+        for (const server of servers) {
+            if (!ns.hasRootAccess(server)) continue;
+            if (!ns.getServerMaxMoney(server)) continue;
+            const minSec = ns.getServerMinSecurityLevel(server);
+            const security = ns.getServerSecurityLevel(server) - minSec;
+            const currMax = Math.floor((ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname())) / ns.getScriptRam('worker.js'));
+
+            ns.print(`Current max: ${currMax}`)
+
+            while (currMax === 0) {
+                await ns.sleep(10000);}
+
+            const threads = Math.min(Math.ceil(security / weakenProgress), currMax);
+            if (threads > 0) _weaken(server, threads);
+        }
+
+        await ns.sleep(10000);
+    }
+}
+
+
+/**
+ * @param {string} target
+ * @param {number} threads
+ * @param {number} timestamp
+ * @returns {function(): boolean}
+ */
+function _weaken(target, threads, timestamp = undefined) {
+    timestamp ||= Date.now() + 1000;
+    const host = ns.getHostname();
+
+    ns.exec('worker.js', host, threads, target, 'weaken', timestamp);
+
+    return () => ns.isRunning('worker.js', host, target, 'weaken', timestamp);
+}
--- updater.js
+++ updater.js
@@ -3,6 +3,7 @@ const baseUrl = 'https://raw.githubusercontent.com/Baizey/BitBurner/1.2.0/';
 export const files = [
     'grow', 'weak', 'hack', 'worker', 'constants', // Basic
     'worm', 'buy_server', 'automater', 'scan', 'connect', 'updater', // Utility
+    'auto_weaken', // automized tool
     'stupid_hack', 'smart_hack' // Hacking stages
 ];
 
